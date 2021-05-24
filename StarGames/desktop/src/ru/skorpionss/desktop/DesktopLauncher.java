e6bcc704938e8ad8c6c3c9a9561e0f28d88eb04c
Maxim <the_prince@sibmail.com>
2021-05-24 19:33:00 +0700

    создание проекта libGDX

---
+++ StarGames/desktop/src/ru/skorpionss/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.skorpionss.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.skorpionss.StarGames;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGames(), config);
+	}
+}
