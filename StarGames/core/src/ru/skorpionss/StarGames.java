6d0133c6240e51e30f38cb99c5ba7f9c5eaf67a6
Maxim <the_prince@sibmail.com>
2021-05-27 06:23:06 +0700

    Растягивание текстуры фона на весь экран

--- StarGames/core/src/ru/skorpionss/StarGames.java
+++ StarGames/core/src/ru/skorpionss/StarGames.java
@@ -1,6 +1,7 @@
 package ru.skorpionss;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,9 +9,12 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class StarGames extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture corabl, fonimg;
+	private float w,h;
 	
 	@Override
 	public void create () {
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
 		corabl = new Texture("corabl.png");
 		fonimg = new Texture("fon.jpg");
@@ -21,7 +25,7 @@ public class StarGames extends ApplicationAdapter {
 
 		ScreenUtils.clear(0,0,0,0);
 		batch.begin();
-		batch.draw(fonimg, 0, 0);
+		batch.draw(fonimg, 0, 0,w,h);
 		batch.draw(corabl, 20, 0);
 		batch.end();
 	}
